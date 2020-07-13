9f3b2eb9d108f71c2441345437cb50be9ff0bbe4
Fernando Tomasio <fernandotomasio@gmail.com>
2020-07-13 20:18:31 -0300

    correções

--- projects/siga-toolkit/src/lib/siga-toolkit.module.ts
+++ projects/siga-toolkit/src/lib/siga-toolkit.module.ts
@@ -6,6 +6,7 @@ import { SigaToolkitComponent } from './siga-toolkit.component';
 @NgModule({
   declarations: [SigaToolkitComponent],
   imports: [
+    FlexLayout
   ],
   exports: [SigaToolkitComponent]
 })
